977e1de171bc94b93f7cff4243f70d71d2d10c18
processid <34098270+processid@users.noreply.github.com>
2022-11-09 22:15:07 +0100

    Update Manager.php
    v1.8.2

--- Manager.php
+++ Manager.php
@@ -101,7 +101,7 @@ Le buffer de débogage est vidé lors de sa lecture : $this->debugTxt(), ou lors
 namespace processid\manager;
 
 /**
- * @version 1.8.1
+ * @version 1.8.2
  */
 abstract class Manager {
     protected $db;
