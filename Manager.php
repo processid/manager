318d2465a472697d1779b03c03e91239b0f39b58
Jean-Marc TILLARD <jm.tillard@process-id.com>
2025-09-22 18:26:31 +0200

    Num version

--- Manager.php
+++ Manager.php
@@ -105,7 +105,7 @@ Le buffer de débogage est vidé lors de sa lecture : $this->debugTxt(), ou lors
 namespace processid\manager;
 
 /**
- * @version 2.1.4
+ * @version 2.2.0
  */
 
 abstract class Manager {
